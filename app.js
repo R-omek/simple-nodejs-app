7fbaa7c3d54659c2de4165c2f2ff2d3c020c575c
roman hrytsiuk <rmngrck97@gmail.com>
2024-02-07 14:16:39 +0200

    add method to get single item

--- app.js
+++ app.js
@@ -4,20 +4,36 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/all-wishes', async (req, res) => {
     try {
-        // Read data from the JSON file
         const data = await fs.readFile('data.json', 'utf8');
         const jsonData = JSON.parse(data);
 
-        // Send the data as a JSON response
         res.json(jsonData);
     } catch (error) {
         console.error('Error reading data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/wish/:id', async (req, res) => {
+    try {
+        const data = await fs.readFile('data.json', 'utf8');
+        const jsonData = JSON.parse(data);
+
+        const wish = jsonData.find((el) => el.id == req.params.id);
+
+        if (!wish) {
+            return res.status(404).json({ error: 'Wish not found' });
+        }
+
+        res.json(wish);
+    } catch (error) {
+        console.error('Error retrieving wish:', error);
         res.status(500).send('Internal Server Error');
     }
 });
